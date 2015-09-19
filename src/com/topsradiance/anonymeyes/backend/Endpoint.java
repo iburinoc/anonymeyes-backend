b40dd69d35ebb8efc6756030a81fc71474a11a5e
Iburinoc <iburinoc@gmail.com>
2015-09-19 05:44:08 -0400

    implemented richards format

--- src/com/topsradiance/anonymeyes/backend/Endpoint.java
+++ src/com/topsradiance/anonymeyes/backend/Endpoint.java
@@ -7,14 +7,14 @@ import java.net.URL;
 public class Endpoint {
 	public static void request(String fname) {
 		try {
-			String addr = "http://localhost/newstream";
+			String addr = "http://localhost/new_video";
 			URL url = new URL(addr);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
 			con.setConnectTimeout(50);
 			con.setRequestMethod("POST");
 			con.setDoOutput(true);
 			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-			wr.writeBytes(fname);
+			wr.writeBytes("filename=" + fname);
 			wr.flush();
 			wr.close();
 			
