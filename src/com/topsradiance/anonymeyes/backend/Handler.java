39610cc23fa4e6ed86ae914afe3c9dcc3b73ab81
Iburinoc <iburinoc@gmail.com>
2015-09-19 05:45:24 -0400

    fixed fname local variable

--- src/com/topsradiance/anonymeyes/backend/Handler.java
+++ src/com/topsradiance/anonymeyes/backend/Handler.java
@@ -25,7 +25,7 @@ public class Handler {
 	public Handler(Long id, Location loc, int width, int height) {
 		this.id = id;
 		lastMessage = System.currentTimeMillis();
-		String fname = "./" + Long.toString(lastMessage / 1000L) + "," + loc.y + "," + loc.x + ".mp4";
+		fname = "./" + Long.toString(lastMessage / 1000L) + "," + loc.y + "," + loc.x + ".mp4";
 		System.out.println("New handler with id " + id + " and fname " + fname + ", dimensions " + width + "," + height);
 		this.img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 		this.imgRaster = this.img.getRaster();
